096c3a9300b5e953983d41cb9a4608af5065d4bd
Kunj Balkrishna Sangani <Kunj_Sangani@ad.infosys.com>
2017-02-11 01:08:21 +0530

    First Commit

---
+++ applicationportfolio/static/applicationportfolio/customchart.js
@@ -0,0 +1,308 @@
+$(document).ready(function(){
+Highcharts.chart('container1', {
+    chart: {
+        type: 'spline',
+        inverted: true
+    },
+    title: {
+        text: 'Atmosphere Temperature by Altitude'
+    },
+    subtitle: {
+        text: 'According to the Standard Atmosphere Model'
+    },
+    xAxis: {
+        reversed: false,
+        title: {
+            enabled: true,
+            text: 'Altitude'
+        },
+        labels: {
+            formatter: function () {
+                return this.value + 'km';
+            }
+        },
+        maxPadding: 0.05,
+        showLastLabel: true
+    },
+    yAxis: {
+        title: {
+            text: 'Temperature'
+        },
+        labels: {
+            formatter: function () {
+                return this.value + '°';
+            }
+        },
+        lineWidth: 2
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        headerFormat: '<b>{series.name}</b><br/>',
+        pointFormat: '{point.x} km: {point.y}°C'
+    },
+    plotOptions: {
+        spline: {
+            marker: {
+                enable: false
+            }
+        }
+    },
+    series: [{
+        name: 'Temperature',
+        data: [[0, 15], [10, -50], [20, -56.5], [30, -46.5], [40, -22.1],
+            [50, -2.5], [60, -27.7], [70, -55.7], [80, -76.5]]
+    }]
+});
+
+	Highcharts.chart('container', {
+    chart: {
+        type: 'area',
+        inverted: true
+    },
+    title: {
+        text: 'Average fruit consumption during one week'
+    },
+    subtitle: {
+        style: {
+            position: 'absolute',
+            right: '0px',
+            bottom: '10px'
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'top',
+        x: -150,
+        y: 100,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+    },
+    xAxis: {
+        categories: [
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+            'Sunday'
+        ]
+    },
+    yAxis: {
+        title: {
+            text: 'Number of units'
+        },
+        labels: {
+            formatter: function () {
+                return this.value;
+            }
+        },
+        min: 0
+    },
+    plotOptions: {
+        area: {
+            fillOpacity: 0.5
+        }
+    },
+    series: [{
+        name: 'John',
+        data: [3, 4, 3, 5, 4, 10, 12]
+    }, {
+        name: 'yash',
+        data: [1, 3, 4, 3, 3, 5, 4]
+    }, {
+        name: 'Kunj',
+        data: [3, 3, 4, 2, 2, 3, 1]
+    }]
+});
+
+Highcharts.setOptions({
+        global: {
+            useUTC: false
+        }
+    });
+
+    Highcharts.chart('container2', {
+        chart: {
+            type: 'spline',
+            animation: Highcharts.svg, // don't animate in old IE
+            marginRight: 10,
+            events: {
+                load: function () {
+
+                    // set up the updating of the chart each second
+                    var series = this.series[0];
+                    setInterval(function () {
+                        var x = (new Date()).getTime(), // current time
+                            y = Math.random();
+                        series.addPoint([x, y], true, true);
+                    }, 1000);
+                }
+            }
+        },
+        title: {
+            text: 'Live random data'
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 150
+        },
+        yAxis: {
+            title: {
+                text: 'Value'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.series.name + '</b><br/>' +
+                    Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                    Highcharts.numberFormat(this.y, 2);
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        exporting: {
+            enabled: false
+        },
+        series: [{
+            name: 'Random data',
+            data: (function () {
+                // generate an array of random data
+                var data = [],
+                    time = (new Date()).getTime(),
+                    i;
+
+                for (i = -19; i <= 0; i += 1) {
+                    data.push({
+                        x: time + i * 1000,
+                        y: Math.random()
+                    });
+                }
+                return data;
+            }())
+        }]
+    });
+Highcharts.chart('container3', {
+
+    chart: {
+        type: 'bubble',
+        plotBorderWidth: 1,
+        zoomType: 'xy'
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    title: {
+        text: 'Sugar and fat intake per country'
+    },
+
+    subtitle: {
+        text: 'Source: <a href="http://www.euromonitor.com/">Euromonitor</a> and <a href="https://data.oecd.org/">OECD</a>'
+    },
+
+    xAxis: {
+        gridLineWidth: 1,
+        title: {
+            text: 'Daily fat intake'
+        },
+        labels: {
+            format: '{value} gr'
+        },
+        plotLines: [{
+            color: 'black',
+            dashStyle: 'dot',
+            width: 2,
+            value: 65,
+            label: {
+                rotation: 0,
+                y: 15,
+                style: {
+                    fontStyle: 'italic'
+                },
+                text: 'Safe fat intake 65g/day'
+            },
+            zIndex: 3
+        }]
+    },
+
+    yAxis: {
+        startOnTick: false,
+        endOnTick: false,
+        title: {
+            text: 'Daily sugar intake'
+        },
+        labels: {
+            format: '{value} gr'
+        },
+        maxPadding: 0.2,
+        plotLines: [{
+            color: 'black',
+            dashStyle: 'dot',
+            width: 2,
+            value: 50,
+            label: {
+                align: 'right',
+                style: {
+                    fontStyle: 'italic'
+                },
+                text: 'Safe sugar intake 50g/day',
+                x: -10
+            },
+            zIndex: 3
+        }]
+    },
+
+    tooltip: {
+        useHTML: true,
+        headerFormat: '<table>',
+        pointFormat: '<tr><th colspan="2"><h3>{point.country}</h3></th></tr>' +
+            '<tr><th>Fat intake:</th><td>{point.x}g</td></tr>' +
+            '<tr><th>Sugar intake:</th><td>{point.y}g</td></tr>' +
+            '<tr><th>Obesity (adults):</th><td>{point.z}%</td></tr>',
+        footerFormat: '</table>',
+        followPointer: true
+    },
+
+    plotOptions: {
+        series: {
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+
+    series: [{
+        data: [
+            { x: 95, y: 95, z: 13.8, name: 'BE', country: 'Belgium' },
+            { x: 86.5, y: 102.9, z: 14.7, name: 'DE', country: 'Germany' },
+            { x: 80.8, y: 91.5, z: 15.8, name: 'FI', country: 'Finland' },
+            { x: 80.4, y: 102.5, z: 12, name: 'NL', country: 'Netherlands' },
+            { x: 80.3, y: 86.1, z: 11.8, name: 'SE', country: 'Sweden' },
+            { x: 78.4, y: 70.1, z: 16.6, name: 'ES', country: 'Spain' },
+            { x: 74.2, y: 68.5, z: 14.5, name: 'FR', country: 'France' },
+            { x: 73.5, y: 83.1, z: 10, name: 'NO', country: 'Norway' },
+            { x: 71, y: 93.2, z: 24.7, name: 'UK', country: 'United Kingdom' },
+            { x: 69.2, y: 57.6, z: 10.4, name: 'IT', country: 'Italy' },
+            { x: 68.6, y: 20, z: 16, name: 'RU', country: 'Russia' },
+            { x: 65.5, y: 126.4, z: 35.3, name: 'US', country: 'United States' },
+            { x: 65.4, y: 50.8, z: 28.5, name: 'HU', country: 'Hungary' },
+            { x: 63.4, y: 51.8, z: 15.4, name: 'PT', country: 'Portugal' },
+            { x: 64, y: 82.9, z: 31.3, name: 'NZ', country: 'New Zealand' }
+        ]
+    }]
+
+});
+});
---
+++ applicationview/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApplicationviewConfig(AppConfig):
+    name = 'applicationview'
